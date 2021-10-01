8aedcc6eb6e03c4c9d561d65e2a03ba13bcbb159
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-01 15:13:00 -0500

    adds employee to array

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 console.log('in js');
-
+// store employees in an array
+const employees = [];
+// document is loaded, run ready()
 $(ready);
 
 function ready() {
@@ -13,4 +15,35 @@ function addEmployee() {
   console.log(`in add e`);
 
   // grab the inputs and save to object
+  let employee = {
+    firstName: $(`#input_first_name`).val(),
+    lastName: $(`#input_last_name`).val(),
+    id: $(`#input_id`).val(),
+    title: $(`#input_title`).val(),
+    salary: $(`#input_salary`).val(),
+  };
+  console.log(employee);
+
+  // push this employee to the employees array
+  employees.push(employee);
+
+  // empty inputs and set focus to first name
+  $(`#input_first_name`).val('');
+  $(`#input_first_name`).focus();
+  $(`#input_last_name`).val('');
+  $(`#input_id`).val('');
+  $(`#input_title`).val('');
+  $(`#input_salary`).val('');
+
+  updateDOM();
+}
+
+function updateDOM() {
+  let tableBody = $(`#table_body`);
+  // clear the table body
+  tableBody.empty();
+  // display to the DOM
+  for (let employee of employees) {
+    console.log(employee);
+  }
 }
