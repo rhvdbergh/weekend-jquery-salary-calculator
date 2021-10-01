202d3438a7c53b5b66deeb5a25ddb8fbb85f6e79
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-01 15:04:49 -0500

    adds not functional addEmployee()

--- client.js
+++ client.js
@@ -4,4 +4,13 @@ $(ready);
 
 function ready() {
   console.log('in jq');
+
+  // add event listeners here
+  $(`#submit_button`).on(`click`, addEmployee);
+}
+
+function addEmployee() {
+  console.log(`in add e`);
+
+  // grab the inputs and save to object
 }
